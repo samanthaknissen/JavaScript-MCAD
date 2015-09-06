9db06b24daacd7f8b027c9727b607c1467081f43
Samantha Baehr <samantha-baehr@Samanthas-MacBook-Pro.local>
2015-09-06 12:36:38 -0500

    Assignment 2

---
+++ assignment2/js/main.js
@@ -0,0 +1,35 @@
+var alabama = document.getElementById('alabama');
+var ourFunction = function(){
+    alert('The capital of Alabama is Montgomery.');
+};
+alabama.addEventListener('mouseover',ourFunction);
+
+var alaska = document.getElementById('alaska');
+var ourFunction = function(){
+    alert('The capital of Alaska is Juneau.');
+};
+alaska.addEventListener('mouseover',ourFunction);
+
+var arizona = document.getElementById('arizona');
+var ourFunction = function(){
+    alert('The capital of Arizona is Phoenix.');
+};
+arizona.addEventListener('mouseover',ourFunction);
+
+var arkansas = document.getElementById('arkansas');
+var ourFunction = function(){
+    alert('The capital of Arkansas is Little Rock.');
+};
+arkansas.addEventListener('mouseover',ourFunction);
+
+var california = document.getElementById('california');
+var ourFunction = function(){
+    alert('The state capital of California is Sacramento.');
+};
+california.addEventListener('mouseenter',ourFunction);
+
+var colorado = document.getElementById('colorado');
+var ourFunction = function(){
+    alert('The state capital of Colorado is Denver.');
+};
+colorado.addEventListener('mouseenter',ourFunction);
